0982ca8e3be553c160626f31fd5218fb9de75c8f
ljsaj <ljsaj@edmac13.cs.umass.edu>
2014-09-30 10:31:37 -0400

    new blank prject

---
+++ simple_app/simple_app/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  simple_app
+//
+//  Created by ljsaj on 9/30/14.
+//  Copyright (c) 2014 _nameorsometing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ simple_app/simple_app/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  simple_app
+//
+//  Created by ljsaj on 9/30/14.
+//  Copyright (c) 2014 _nameorsometing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
